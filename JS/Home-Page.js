bdd23f76e2583c15f5944382c9088884cd627340
Alaa Waleed <aw827948@gmail.com>
2025-03-13 01:24:45 +0200

    Home Page image parallex & linked the nav bar with appoinment page

--- JS/Home-Page.js
+++ JS/Home-Page.js
@@ -1,8 +1,17 @@
 window.addEventListener("scroll", function () {
     let navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
+    if (window.scrollY > 20) {
         navbar.style.opacity = "0.8";  // Slightly faded
     } else {
         navbar.style.opacity = "1";  // Fully visible
     }
+});
+
+
+window.addEventListener("scroll", function() {
+    let image = document.querySelector(".Stethoscope");
+    let scrollPosition = window.scrollY;
+
+    // Moves the image to the right on scroll down and back on scroll up
+    image.style.transform = `translateX(${scrollPosition * 0.7}px) rotate(45deg)`;
 });
